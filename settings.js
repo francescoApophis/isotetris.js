70966135178dc6cf9a1184334352bff9231cf763
Francesco <francescopaul789@gmail.com>
2024-08-19 04:01:23 +0200

    Add fonts/font-sizes for debug-mode and pause rendering

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas');
 
 export const ROWS = 10;
 export const COLS = 20;
-export const BLOCK_SIZE = 20;
+export const BLOCK_SIZE = 25;
 
 export const C_W = canvas.width;
 export const C_H = canvas.height;
@@ -13,5 +13,11 @@ export const FPS_INTERVAL = 1000 / FPS;
 export const LEVEL_FPS = 5; 
 export const LEVEL_FPS_INTERVAL = 1000 / LEVEL_FPS;
 
-// JLOSZI
-export const SHAPE_TYPES = 'JLOSZIT';
+// JLOSZIT
+export const SHAPE_TYPES = 'JLOSZIT'; // can be either lower or upper case 
+
+export const DEBUG_FONT = 'sans-serif';
+export const DEBUG_FONT_SIZE = 10;
+
+export const PAUSE_FONT = 'Arial';
+export const PAUSE_FONT_SIZE = 25;
