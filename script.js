d6d3863fcfb580b8e56563b096500eac81159c59
Francesco <francescopaul789@gmail.com>
2024-08-29 07:15:37 +0200

    MINOR: load 'iso-assets4.png' as current iso txtr

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 import {Shape} from "./shape.js";
 import {FPS, FPS_INTERVAL, LEVEL_FPS, LEVEL_FPS_INTERVAL, C_W, C_H, ROWS, COLS, BLOCK_SIZE} from "./settings.js";
-// import {draw_table, render_table, clear_matrix, clear_row, drop_previous_shapes, render_isometric_table} from "./utils.js";
-
-
 import {Game} from "./game.js";
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+// this two ctxs are for debugging the table at different states at the same time
+// const canvas2 = document.getElementById('canvas2');
+// const ctx2 = canvas2.getContext('2d');
+// const canvas3 = document.getElementById('canvas3');
+// const ctx3 = canvas3.getContext('2d');
+
 const pixel_art_txr = new Image();
 const isometric_txr = new Image();
 pixel_art_txr.src = "./assets/square2.png";
-isometric_txr.src = "./assets/iso-assets3.png";
-
+isometric_txr.src = "./assets/iso-assets4.png";
 
 const game = new Game(ctx, canvas, pixel_art_txr, isometric_txr);
 
@@ -20,7 +22,6 @@ const debug_btn = document.getElementById('debug_btn');
 const iso_btn = document.getElementById('iso_btn');
 debug_btn.onclick = () => game.debug_mode = !game.debug_mode;
 iso_btn.onclick = () => game.iso_mode = !game.iso_mode;
-
 
 document.addEventListener('keydown', (e)=>{
   switch(e.key){
