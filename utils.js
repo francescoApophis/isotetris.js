76adadd20e715580e9dd2c27556188c7e645b7a0
Francesco <francescopaul789@gmail.com>
2024-08-29 09:01:47 +0200

    MINOR: create Texture classes

--- utils.js
+++ utils.js
@@ -1,31 +1,21 @@
-import {ROWS, COLS, BLOCK_SIZE} from "./settings.js";
+
+export class Texture {
+  constructor(path, crop_width, crop_height){
+    // in drawImage() I have to use Texture.img
+    this.img = new Image();
+    this.img.src = path;
+    this.crop_w = crop_width;
+    this.crop_h = crop_height
+  }
+}
+
+export class IsoBlockTexture extends Texture {
+  constructor (path, crop_width, crop_height, tlm_vertex, resize){
+    super(path, crop_width, crop_height)
+    this.tlm_vertex = tlm_vertex; // Top Left Most vertex of the block
+    // iso images are much bigger so they need to be scaled down
+    this.resize = resize; 
+  }
+}
 
 
-
-
-// function cl(block, table, then){
-  // id = requestAnimationFrame(() => {
-    // cl(block, table, then);
-  // });
-
-  // let y = 0;
-
-  // for (; y < ROWS; y++){
-    // if (table[block.x * ROWS + y] != 0) break;
-  // }
-
-  // for (; y < ROWS; y++){
-    // let now = Date.now();
-    // let elapsed = now - then;
-
-    // if (elapsed >= 18){
-      // then = now - (elapsed % 18);
-
-      // table[block.x * ROWS + y] = 0; 
-
-      // if (table[block.x * ROWS + y] == 0 && y == ROWS - 1) {
-        // cancelAnimationFrame(id);
-      // } 
-    // }
-  // }
-// }
